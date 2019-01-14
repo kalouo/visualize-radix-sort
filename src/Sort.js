9bf18dc4e100a5cf483cf29d2fec0f4337709dcd
okalouti <kalouo@pm.me>
2019-01-14 15:50:24 +0900

    implements radix sort and passes corresponding tests

--- src/Sort.js
+++ src/Sort.js
@@ -2,14 +2,35 @@ class SomeKindOfSort {
   constructor(array) {
     this.array = array;
   }
-  sort() {
-    console.log("sort!");
-    console.log("sort!");
-    console.log("sort!");
+  countDigits(num) {
+    return Number(String(num).length);
   }
-
-  returnValue(value) {
-    return value;
+  getDigit(num, i) {
+    return i + 1 > String(num).length
+      ? 0
+      : Number(String(num)[String(num).length - i - 1]);
+  }
+  countMax(array) {
+    let maxDigits = 0,
+      length;
+    for (let number of array) {
+      length = this.countDigits(number);
+      maxDigits = Math.max(maxDigits, length);
+    }
+    return maxDigits;
+  }
+  sortRadix(numbers) {
+    const maxDigits = this.countMax(numbers);
+    let digitSort, digit;
+    for (let k = 0; k < maxDigits; k++) {
+      digitSort = Array.from({ length: 10 }, () => []);
+      for (let i = 0; i < numbers.length; i++) {
+        digit = this.getDigit(numbers[i], k);
+        digitSort[digit].push(numbers[i]);
+      }
+      numbers = [].concat(...digitSort);
+    }
+    return numbers;
   }
 }
 
--- test/_sort.js
+++ test/_sort.js
@@ -1,11 +1,32 @@
 const Sort = require("../src/Sort");
 const { expect } = require("chai");
 
-describe("Sort", () => {
+describe("Radix Sort", () => {
   it("should be a function", () => {
     expect(Sort).to.be.a("function");
   });
   it("should have a sort method", () => {
-    expect(Sort.prototype.sort).to.be.a("function");
+    expect(Sort.prototype.sortRadix).to.be.a("function");
+  });
+  it("should have a helper function countDigits that returns the number of digits in a number", () => {
+    expect(Sort.prototype.countDigits(12345)).to.equal(5);
+  });
+  it("should have a helper function getDigit that returns the digit of at a given position", () => {
+    expect(Sort.prototype.getDigit(12345, 0)).to.equal(5);
+    expect(Sort.prototype.getDigit(12345, 1)).to.equal(4);
+    expect(Sort.prototype.getDigit(12345, 2)).to.equal(3);
+    expect(Sort.prototype.getDigit(12345, 3)).to.equal(2);
+    expect(Sort.prototype.getDigit(12345, 4)).to.equal(1);
+    expect(Sort.prototype.getDigit(7, 4)).to.equal(0);
+  });
+  it("should have a helper function countMax that returns the maximum number of digits in a number array", () => {
+    const arr = [1556, 4, 3556, 593, 408, 4386, 902, 7, 8157, 86, 9637, 29];
+    expect(Sort.prototype.countMax(arr)).to.equal(4);
+  });
+  it("should sort an  array of numbers in ascending order", () => {
+    const array = [1556, 4, 3556, 593, 408, 902, 7, 86, 29];
+    const result = Sort.prototype.sortRadix(array);
+    const expected = [4, 7, 29, 86, 408, 593, 902, 1556, 3556];
+    expect(...result).to.equal(...expected);
   });
 });
